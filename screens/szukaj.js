e9b94c7adc6493974ac9851a64cb0ee80a94219c
Łukasz Chwalisz <lukaszchwalisz@mx>
2022-08-02 16:08:24 +0200

    change in branch test

--- screens/szukaj.js
+++ screens/szukaj.js
@@ -1,127 +1,64 @@
-// Searching using Search Bar Filter in React Native List View
-// https://aboutreact.com/react-native-search-bar-filter-on-listview/
-// import React in our code
-import React, { useState, useEffect } from 'react';
-// import all the components we are going to use
-import {
-  SafeAreaView,
-  Text,
-  StyleSheet,
-  View,
-  FlatList,
-  TextInput,
-} from 'react-native';
+import React from 'react';
+import { View, Text, FlatList } from 'react-native';
+import 'react-native-gesture-handler';
+import { styles } from '../../styles/global.js';
+import { Divider, Surface } from 'react-native-paper';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
 
-import { customData } from '../data/dane.json'; 
+export default function Szukaj({ navigation, route }) {
 
-const Szukaj = () => {
-  const [search, setSearch] = useState('');
-  const [filteredDataSource, setFilteredDataSource] = useState([]);
-  const [masterDataSource, setMasterDataSource] = useState([]);
-
-  useEffect(() => {
-
-    fetch(customData)
-      .then((response) => response.json())
-      .then((responseJson) => {
-        setFilteredDataSource(responseJson);
-        setMasterDataSource(responseJson);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-
-  }, []);
-
-  const searchFilterFunction = (text) => {
-    // Check if searched text is not blank
-    if (text) {
-      // Inserted text is not blank
-      // Filter the masterDataSource and update FilteredDataSource
-      const newData = masterDataSource.filter(function (item) {
-        // Applying filter for the inserted text in search bar
-        const itemData = item.title
-          ? item.title.toUpperCase()
-          : ''.toUpperCase();
-        const textData = text.toUpperCase();
-        return itemData.indexOf(textData) > -1;
-      });
-      setFilteredDataSource(newData);
-      setSearch(text);
-    } else {
-      // Inserted text is blank
-      // Update FilteredDataSource with masterDataSource
-      setFilteredDataSource(masterDataSource);
-      setSearch(text);
+  const DATA = [
+    {
+      id: 'p1',
+      title: 'Stres w pracy.',
+    },
+    {
+      id: 'p2',
+      title: 'Wypalenie zawodowe.',
+    },
+    {
+      id: 'p3',
+      title: 'Konlifkt.',
+    },
+    {
+      id: 'p4',
+      title: 'Mobbing.',
+    },
+    {
+      id: 'p5',
+      title: 'Uprzedzenia, stereotypy, dyskryminacja.',
     }
-  };
+  ];
 
-  const ItemView = ({ item }) => {
+
+  const renderItem = ({ item }) => (
+        <TouchableWithoutFeedback style={styles.button} onPress={() => {
+          navigation.push(item.id)
+        }}>
+          <Surface style={styles.surface} elevation={2}>
+            <Text style={styles.tabs_text}>{item.title}</Text>
+          </Surface>
+        </TouchableWithoutFeedback>
+  );
+
+  const Header = () => {
     return (
-      // Flat List Item
-      <Text style={styles.itemStyle} onPress={() => getItem(item)}>
-        {item.id}
-        {'.'}
-        {item.title.toUpperCase()}
-      </Text>
-    );
-  };
+      <Surface style={styles.surface} elevation={2}>
+        <Text style={styles.tabs_bold}>Moje zdrowie </Text>
+        <Text style={styles.tabs_text}>Informacje na temat zdrowotnych skutków działań mobbingowych w miejscu pracy.
+        </Text>
+        </Surface>
 
-  const ItemSeparatorView = () => {
-    return (
-      // Flat List Item Separator
-      <View
-        style={{
-          height: 0.5,
-          width: '100%',
-          backgroundColor: '#C8C8C8',
-        }}
-      />
-    );
-  };
-
-  const getItem = (item) => {
-    // Function for click on an item
-    alert('Id : ' + item.id + ' Title : ' + item.title);
+    )
   };
 
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View style={styles.container}>
-        <TextInput
-          style={styles.textInputStyle}
-          onChangeText={(text) => searchFilterFunction(text)}
-          value={search}
-          underlineColorAndroid="transparent"
-          placeholder="Search Here"
-        />
-        <FlatList
-          data={filteredDataSource}
-          keyExtractor={(item, index) => index.toString()}
-          ItemSeparatorComponent={ItemSeparatorView}
-          renderItem={ItemView}
-        />
-      </View>
-    </SafeAreaView>
-  );
+     <FlatList 
+      ListHeaderComponent={Header} 
+      data={DATA}
+      renderItem={renderItem}
+      keyExtractor={item => item.id}
+      />
+  )
 };
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'white',
-  },
-  itemStyle: {
-    padding: 10,
-  },
-  textInputStyle: {
-    height: 40,
-    borderWidth: 1,
-    paddingLeft: 20,
-    margin: 5,
-    borderColor: '#009688',
-    backgroundColor: '#FFFFFF',
-  },
-});
-
-export default Szukaj;
