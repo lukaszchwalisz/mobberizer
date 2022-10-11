5bc525f19a3c46cceead97e49a09f7c805c9fd85
Łukasz Chwalisz <lukaszchwalisz@proton.me>
2022-10-11 23:31:04 +0200

    background in searchbar

--- data/searchHeader.js
+++ data/searchHeader.js
@@ -8,14 +8,26 @@ const Header = ({onSearch}) => {
         <Text style={styles.tabs_title}>FAQ </Text>
         <Text style={styles.tabs_subtitle}>Najczęściej zadawane pytania dotyczące mobbingu
         w miejscu pracy.{"\n"}{"\n"}
-        Stan prawny: 04.05.2022 r.
+        Stan prawny: 04.05.2022 r.{"\n"}
         </Text>
-
+        <View
+          style={{
+            borderRadius: 5,
+            padding: 3,
+            backgroundColor: "#f5f5dc", 
+            fontSize: 17,
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
         <TextInput
           placeholder="Szukaj.."
           onChangeText={onSearch}
           style={{ flex: 1 }}
         />
+
+        </View>
+
 
       </View>
 
