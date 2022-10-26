5c8e1df34be4e38164dded31f6352370ff67d740
Łukasz Chwalisz <lukaszchwalisz@proton.me>
2022-10-26 16:59:19 +0200

    test phone-call

--- data/searchItems.js
+++ data/searchItems.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Text,Linking } from "react-native";
+import { Text } from "react-native";
 import { styles } from '../styles/global.js';
 import { Surface } from 'react-native-paper';
 import 'react-native-gesture-handler';
 import { ScrollView, TouchableWithoutFeedback } from 'react-native-gesture-handler';
 import { useNavigation } from "@react-navigation/native";
 import A from 'react-native-a';
-
+import call from 'react-native-phone-call';
 
 const RenderItem = ({ data }) => {
     const navigation = useNavigation();
@@ -26,15 +26,25 @@ const RenderItem = ({ data }) => {
 }
 
 const RenderItem_s1 = ({ data }) => {
-    const navigation = useNavigation();
-    
+    // const navigation = useNavigation();
+   
+    const args = {
+        number: data.tel, // String value with the number to call
+        prompt: false, // Optional boolean property. Determines if the user should be prompted prior to the call 
+        skipCanOpen: true // Skip the canOpenURL check
+    }
+  
     return (  
         <ScrollView > 
         <Surface style={styles.surface} elevation={2}>
             <Text style={styles.tabs_text}>{data.title}</Text>
             <Text style={styles.tabs_text}>adres:  {data.adres}</Text>
+            <TouchableWithoutFeedback style={styles.button} onPress={() => {
+                call(args).catch(console.error)
+            }}> 
             <Text style={styles.tabs_text}>tel.:  {data.tel}</Text>
-            <Text onPress={()=>{Linking.openURL('tel://{data.tel}')}} style={styles.tabs_text}>{data.tel}</Text>
+            </TouchableWithoutFeedback>
+
             <Text style={styles.tabs_text}>email:  {data.email}</Text>
             <Text style={styles.tabs_text}>www:  <A href={data.www}>{data.www}</A></Text>
             <Text style={styles.tabs_text}>epuap:  {data.epuap}</Text>
@@ -44,14 +54,27 @@ const RenderItem_s1 = ({ data }) => {
 }
 
 const RenderItem_s2 = ({ data }) => {
-    const navigation = useNavigation();
-    
+    // const navigation = useNavigation();
+    const args = {
+        number: data.tel, // String value with the number to call
+        prompt: false, // Optional boolean property. Determines if the user should be prompted prior to the call 
+        skipCanOpen: true // Skip the canOpenURL check
+    }
+
     return (  
         <ScrollView > 
 
         <Surface style={styles.surface} elevation={2}>
             <Text style={styles.tabs_text}>{data.title}</Text>
+            <TouchableWithoutFeedback style={styles.button} onPress={() => {
+                call(args).catch(console.error)
+            }}> 
             <Text style={styles.tabs_text}>tel.:  {data.tel}</Text>
+            </TouchableWithoutFeedback>
+
+            {/* <Text style={styles.tabs_text}>tel.:  {data.tel}</Text> */}
+
+
             <Text style={styles.tabs_text}>email:  {data.email}</Text>
             <Text style={styles.tabs_text}>www:  <A href={data.www}>{data.www}</A></Text>
             <Text style={styles.tabs_text}>PSSE:  {data.psse}</Text>
