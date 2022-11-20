d360ce9064a5a0fedda04f632598fa9a060baf76
Łukasz Chwalisz <lukasz.chwalisz@gmail.com>
2022-11-20 14:50:47 +0100

    try fix key unique problem

--- data/pip.js
+++ data/pip.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const PIP = [
 {
+	id: '0',
 	title:'Państwowa Inspekcja Pracy Okręgowy Inspektorat Pracy w Warszawie',
 	adres:'ul. Płocka 11/13, 01-231 Warszawa ',
 	tel:'224251194',
@@ -11,6 +12,7 @@ const PIP = [
 	tag: 'mazowieckie, Warszawa',
 },
 {
+	id: '1',
 	title:'Państwowa Inspekcja Pracy Okręgowy Inspektorat Pracy we Wrocławiu',
 	adres:'ul. Zielonego Dębu 22, 51-621 Wrocław',
 	tel: '(71) 37 10 430, 71 37 10 468',
@@ -20,6 +22,7 @@ const PIP = [
 	tag: 'dolnośląskie, Wrocław',
 },
 {
+	id: '2',
 	title:'Państwowa Inspekcja Pracy Okręgowy Inspektorat Pracy w Zielonej Górze',
 	adres:'ul. Dekoracyjna 8, 65-722 Zielona Góra',
 	tel:'(68) 451 39 00',
@@ -29,6 +32,8 @@ const PIP = [
 	tag: 'lubuskie, Zielona Góra',
 },
 {
+
+	id: '3',
 	title:'Państwowa Inspekcja Pracy Ośrodek Szkolenia Państwowej Inspekcji Pracy im. Profesora Jana Rosnera we Wrocławiu ',
 	adres:'ul. Mikołaja Kopernika 5, 51-622 Wrocław ',
 	tel:'(71) 37 10 404',
@@ -38,6 +43,7 @@ const PIP = [
 	tag: 'dolnośląskie, Wrocław',
 },
 {
+	id: '4',
 	title:'Państwowa Inspekcja Pracy Okręgowy Inspektorat Pracy w Białymstoku',
 	adres:'ul. Fabryczna 2, 15-483 Białystok',
 	tel:'(85) 678 57 00',
@@ -47,112 +53,124 @@ const PIP = [
 	tag: 'podlaskie, Białystok',
 },
 {
+	id: '5',
 	title: 'Państwowa Inspekcja Pracy Okręgowy Inspektorat Pracy w Krakowie',
 	adres: 'pl. Szczepański 5, 31-011 Kraków',
 	tel: '(12) 424 04 50',
 	email: 'kancelaria@krakow.pip.gov.pl',
 	www:'https://krakow.pip.gov.pl/pl/',
 	epuap: '/gsiodlarzepuap/SkrytkaESP',
-	tag:'małopolskie, Kraków'
+	tag:'małopolskie, Kraków',
 },
 {
+	id: '6',
 	title: 'Państwowa Inspekcja Pracy Okręgowy Inspektorat Pracy w Opolu',
 	adres: 'ul. Oleska 121 „C”, 45-231 Opole', 
 	tel: '(77) 47 00 900',
 	email: 'kancelaria@opole.pip.gov.pl',
 	www:'https://opole.pip.gov.pl/pl/',
 	epuap: '/oipople/SkrytkaESP',
-	tag:'opolskie, Opole' 
+	tag:'opolskie, Opole',
 },
 {
+	id: '7',
 	title: 'Państwowa Inspekcja Pracy Okręgowy Inspektorat Pracy w Kielcach',
 	adres: 'Al. Tysiąclecia Państwa Polskiego nr 4, 25-314 Kielce', 
 	tel: '(41) 343 82 76',
 	email: 'kancelaria@kielce.pip.gov.pl',
 	www:'https://kielce.pip.gov.pl/pl/',
 	epuap: '/OIPKielce/SkrytkaESP',
-	tag: 'świętokrzyskie, Kielce'
+	tag: 'świętokrzyskie, Kielce',
 },
 {
+	id: '8',
 	title: 'Państwowa Inspekcja Pracy Okręgowy Inspektorat Pracy w Gdańsku',
 	adres: 'ul. Okopowa 7, 80-819 Gdańsk', 
 	tel: '(58) 520 18 22 lub 23 ',
 	email: 'kancelaria@gdansk.pip.gov.pl',
 	www:'https://gdansk.pip.gov.pl/pl/',
 	epuap: '/OIPGD/skrytka',
-	tag: 'pomorskie, Gdańsk'
+	tag: 'pomorskie, Gdańsk',
 },
 {
+	id: '9',
 	title: 'Państwowa Inspekcja Pracy Okręgowy Inspektorat Pracy w Poznaniu',
 	adres: 'ul. Marcelińska 90, 60-324 Poznań', 
 	tel: '61 628 40 00',
 	email: 'kancelaria@poznan.pip.gov.pl',
 	www:'https://poznan.pip.gov.pl/pl/',
 	epuap: '/oippoznan/SkrytkaESP',
-	tag: 'wielkopolskie, Poznań'
+	tag: 'wielkopolskie, Poznań',
 },
 {
+	id: '10',
 	title: 'Państwowa Inspekcja Pracy Okręgowy Inspektorat Pracy w Rzeszowie',
 	adres: 'ul. Gen. St. Maczka 4, 35-234 Rzeszów', 
 	tel: '(17) 717 20 00',
 	email: 'kancelaria@rzeszow.pip.gov.pl',
 	www:'https://rzeszow.pip.gov.pl/pl/',
 	epuap: 'OIP/SkrytkaESP',
-	tag: 'podkarpackie, Rzeszów'
+	tag: 'podkarpackie, Rzeszów',
 },
 {
+	id: '11',
 	title: 'Państwowa Inspekcja Pracy Okręgowy Inspektorat Pracy w Szczecinie',
 	adres: 'ul. Pszczelna 7, 71-663 Szczecin', 
 	tel: '(91) 431 19 30',
 	email: 'kancelaria@szczecin.pip.gov.pl',
 	www:'https://szczecin.pip.gov.pl/pl/',
 	epuap: '/oipszczecin/SkrytkaESP',
-	tag: 'zachodnopomorskie, Szczecin'
+	tag: 'zachodnopomorskie, Szczecin',
 },
 {
+	id: '12',
 	title: 'Państwowa Inspekcja Pracy Okręgowy Inspektorat Pracy w Bydgoszczy',
 	adres: 'Plac Piastowski 4a, 85-012 Bydgoszcz', 
 	tel: '(52) 320 24 00',
 	email: 'kancelaria@bydgoszcz.pip.gov.pl',
 	www:'https://bydgoszcz.pip.gov.pl/pl/',
 	epuap: '/PIPOIPBydgoszcz/SkrytkaESP',
-	tag: 'kujawsko-pomorskie, Bydgoszcz'
+	tag: 'kujawsko-pomorskie, Bydgoszcz',
 },
 {
+	id: '13',
 	title: 'Państwowa Inspekcja Pracy Okręgowy Inspektorat Pracy w Olsztynie',
 	adres: 'ul. Kopernika 29, 10-512 Olsztyn', 
 	tel: '(89) 527 42 75',
 	email: 'kancelaria@olsztyn.pip.gov.pl',
 	www:'https://olsztyn.pip.gov.pl/pl/',
 	epuap: '/OIP10/SkrytkaESP',
-	tag: 'warmińsko-mazurskie, Olsztyn'
+	tag: 'warmińsko-mazurskie, Olsztyn',
 },
 {
+	id: '14',
 	title: 'Państwowa Inspekcja Pracy Okręgowy Inspektorat Pracy w Katowicach',
 	adres: 'ul. Owocowa 6-6a, 40-158 Katowice', 
 	tel: '(32) 60 41 208',
 	email: 'kancelaria@katowice.pip.gov.pl',
 	www:'https://katowice.pip.gov.pl/pl/',
 	epuap: '/oipkatowice/SkrytkaESP',
-	tag: 'śląskie, Katowice'
+	tag: 'śląskie, Katowice',
 },
 {
+	id: '15',
 	title: 'Państwowa Inspekcja Pracy Okręgowy Inspektorat Pracy w Lublinie',
 	adres: 'Al. Piłsudskiego 13, 20-011 Lublin', 
 	tel: '(81) 53 71 131',
 	email: 'kancelaria@lublin.pip.gov.pl',
 	www:'https://lublin.pip.gov.pl/pl/',
 	epuap: '/PIP/SkrytkaESP',
-	tag: 'lubelskie, Lublin'
+	tag: 'lubelskie, Lublin',
 },
 {
+	id: '16',
 	title: 'Państwowa Inspekcja Pracy Okręgowy Inspektorat Pracy w Łodzi',
 	adres: 'al. Kościuszki 123, 90-441 Łódź', 
 	tel: '(42) 636 23 13',
 	email: 'kancelaria@lodz.pip.gov.pl',
 	www:'https://lodz.pip.gov.pl/pl/',
 	epuap: '/OIP09/SkrytkaESP',
-	tag: 'łódzkie, Łódź'
+	tag: 'łódzkie, Łódź',
 }
 
 ];
