444cec3619681e225475b02aac794f649f67382d
Łukasz Chwalisz <lukasz.chwalisz@gmail.com>
2023-05-27 21:21:15 +0200

    create dict.js

---
+++ data/dict.js
@@ -0,0 +1,119 @@
+const dict = [
+    {
+        id: '1',
+        title:'Pracoholizm',
+        content: 'uzależnienie psychiczne od pracy, objawiające się obsesyjną i wewnętrzną potrzebą ciągłego wykonywania pracy kosztem innych czynności, również rodziny, snu i odpoczynku',
+    },
+    {
+        id: '2',
+        title:'Wypalenie zawodowe',
+        content: 'ostre zaburzenie stresowe bądź reakcja, cechująca się wyczerpaniem wynikającym z przepracowania oraz lękiem, zmęczeniem, bezsennością, depresją i gorszymi wynikami w pracy',
+    },
+    {
+        id: '3',
+        title:'Zaburzenia adaptacyjne',
+        content: 'zaburzenia emocjonalne i zachowania występujące w odpowiedzi na stresujące wydarzenia lub sytuacje życiowe',
+    },
+    {
+        id: '4',
+        title:'Ostre zaburzenie stresowe',
+        content: 'zaburzenie emocjonalne występujące po doświadczeniu lub świadkowaniu skrajnie stresującego wydarzenia lub sytuacji',
+    },
+    {
+        id: '5',
+        title:'Zaburzenie stresowe pourazowe - PTSD',
+        content: 'zaburzenie psychiczne będące formą reakcji na skrajnie stresujące wydarzenie (traumę), które przekracza zdolności danej osoby do radzenia sobie i adaptacji',
+    },
+    {
+        id: '6',
+        title:'Depresja',
+        content: 'zaburzenie nastroju charakteryzujące się obniżonym nastrojem, utratą zainteresowań i przyjemności z życia oraz zmniejszeniem aktywności fizycznej i psychicznej',
+    },
+    {
+        id: '7',
+        title:'Nerwica',
+        content: 'choroba psychiczna charakteryzująca się występowaniem napadów lęku lub niepokoju',
+    },
+    {
+        id: '8',
+        title:'Stres',
+        content: 'reakcja organizmu na bodziec zewnętrzny lub wewnętrzny, który wymaga dostosowania się do nowych warunków lub sytuacji',
+    },
+    {
+        id: '9',
+        title:'Lęk',
+        content: 'jest to emocja odczuwana w sytuacjach zagrożenia lub niepewności',
+    },
+    {
+        id: '10',
+        title:'Zaburzenia lękowe',
+        content: 'to grupa chorób psychicznych charakteryzujących się występowaniem nadmiernego lęku lub niepokoju',
+    },
+    {
+        id: '11',
+        title:'Zaburzenia psychosomatyczne',
+        content: 'zaburzenia somatyczne (fizyczne) wynikające z czynników psychologicznych lub emocjonalnych',
+    },
+    {
+        id: '12',
+        title:'Myśli samobójcze',
+        content: 'myśli o samobójstwie lub planowanie samobójstwa',
+    },
+    {
+        id: '13',
+        title:'Koszmary nocne',
+        content: 'intensywne i nieprzyjemne sny, które często budzą z uczuciem lęku lub paniki',
+    },
+    {
+        id: '14',
+        title:'Bezsenność',
+        content: 'zaburzenie snu polegające na trudnościach z zaśnięciem lub utrzymaniem snu',
+    },
+    {
+        id: '15',
+        title:'Niezdolność do pracy',
+        content: 'brak zdolności do wykonywania pracy zawodowej z powodu choroby lub urazu',
+    },
+    {
+        id: '16',
+        title:'Trwały uszczerbek na zdrowiu',
+        content: 'uszkodzenie ciała lub umysłu, które prowadzi do trwałych zmian w funkcjonowaniu organizmu',
+    },
+    {
+        id: '17',
+        title:'Mobbing',
+        content: 'długotrwałe i systematyczne nękanie lub zastraszanie pracownika przez przełożonego lub współpracowników',
+    },
+    {
+        id: '18',
+        title:'Konflikt',
+        content: 'sytuacja, w której dwie lub więcej stron mają sprzeczne cele lub interesy',
+    },
+    {
+        id: '19',
+        title:'Dyskryminacja',
+        content: 'traktowanie jednej osoby lub grupy osób w sposób gorszy niż innych z powodu ich przynależności do określonej grupy społecznej np. ze względu na płeć, wiek, orientację seksualną, pochodzenie etniczne',
+    },
+    {
+        id: '20',
+        title:'Molestowanie seksualne',
+        content: 'niepożądane zachowanie o charakterze seksualnym, które narusza godność osoby i powoduje jej poczucie upokorzenia lub zastraszenia',
+    },
+    {
+        id: '21',
+        title:'Stalking',
+        content: 'uporczywe i niepożądane śledzenie czyjejś osoby, które powoduje u niej lęk lub poczucie zagrożenia',
+    },
+    {
+        id: '22',
+        title:'Bullying',
+        content: 'długotrwałe i systematyczne nękanie lub zastraszanie jednej osoby przez inną osobę lub grupę osób',
+    },
+    {
+        id: '23',
+        title:'Organizacje turkusowe',
+        content: 'to model zarządzania, w którym stopień świadomości zarządzania organizacją jest najbardziej zdecentralizowany i równy u wszystkich członków organizacji',
+    },
+    ];
+    
+    export { dict };
