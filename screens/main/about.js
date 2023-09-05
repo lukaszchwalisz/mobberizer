b8dbf154b4d00a0ccafb0681c345b04800837f1c
Łukasz Chwalisz <lukasz.chwalisz@gmail.com>
2023-09-05 20:07:46 +0200

    new content in about.js

--- screens/main/about.js
+++ screens/main/about.js
@@ -16,23 +16,20 @@ export default function About({navigation}) {
             </View>
             <Text style={styles.tabs_text}> 
             <Text style={styles.tabs_bold}>Mobberizer </Text> 
-             to aplikacja mobilna przeznaczona dla osób, które spotkały się z
-             niewłaściwymi zachowaniami w pracy o charakterze mobbingowym i poszukują pomocy psychologicznej i
-             prawnej, a także podstawowych informacji na temat mobbingu oraz porad jak zachować się z trudnej,
-             stresującej sytuacji. {"\n"}
+            to aplikacja mobilna dedykowana osobom, które spotkały się z niewłaściwymi zachowaniami w pracy o charakterze mobbingowym;
+            chcą zdiagnozować zaistniałą sytuację w pracy; poszukują pomocy psychologicznej i prawnej oraz instytucjonalnej.
+             {"\n"}
+
              </Text>
-             <Text style={styles.tabs_text}>Wiele osób pada ofiarą zjawiska mobbingu w pracy,
-             pomimo tego ma trudności w określeniem tego czy zasadnym jest uznać zachowanie
-             przełożonego lub współpracowników za zachowania mobbingowe. Najczęściej w pierwszym
-             odruchu odrzuca taką myśl, stara się jak najlepiej wykonywać zadania, realizować się
-             w pracy, mając nadzieję, że sytuacja poprawi się. Jednak nie dość, że nie poprawia się
-             to zachowania mobbingowe się nasilają. Wtedy stopniowo pojawia się poczucie bezradności,
-             braku wpływu, zaczynają się problemy zdrowotne.{"\n"}</Text>
+             <Text style={styles.tabs_text}>
+             <Text style={styles.tabs_bold}>Mobberizer </Text> 
+             to narzędzie wsparcia dla osób zastanawiających się od czego zacząć, w jaki sposób skutecznie przeciwdziałać mobbingowi.
+              {"\n"}</Text>
              
             <Text style={styles.tabs_text}>
-            W takim momencie osoby najczęściej zaczynają poszukiwać pomocy poza zakładem pracy. Aplikacja
-            stara się przekazać podstawową pigułkę informacji potrzebną w sytuacji kryzysowej w pracy.
-            Wskazuje też na kolejne kroki jakie może podjąć w celu przeciwdziałania mobbingowi.      
+            Aplikacja jest 
+            <Text style={styles.tabs_italic}> bezpłatna</Text> 
+            , nie zawiera reklam, jest przyjazna dla użytkownika.
               {"\n"}</Text>
 
              </View>
