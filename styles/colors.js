a5039612fb2ce8c21587e3f2692c261eca84f0e5
Łukasz Chwalisz <lukasz.chwalisz@gmail.com>
2023-10-17 19:51:19 +0200

    add colors

--- styles/colors.js
+++ styles/colors.js
@@ -8,14 +8,15 @@ const bgdark = '#2f2f2f';
 const white = '#fff';
 const blue = '#2483DB'
 const bluish = '#edf6f9'
-
+const bluish1 = '#5FE2FF'
+const bluish2 = '#3B97EC'
 
 const darkmode = {
     dark0,dark1,dark2,bgdark
 };
 
 const lightmode = {
-    white, blue, bluish
+    white, blue, bluish, bluish1, bluish2, 
 };
 
 
